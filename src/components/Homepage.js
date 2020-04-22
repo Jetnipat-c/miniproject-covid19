a839a4bab6e6d8b5039d4b30f2442bb1998e848b
alonecheer <alonecheer@hotmail.com>
2020-04-23 01:27:07 +0700

    home page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import fire from '../config/fire';
+import React from 'react'
+import fire from '../config/fire'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './homePage.css'
 import { Nav, Navbar, Button, Form, FormControl } from 'react-bootstrap'
 function HomePage () {
-    const logout = () => {
-        fire.auth().signOut();
-    }
-    return(
-        <div>
-           <Navbar>
-        <Navbar.Brand href='/'>Navbar with text</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className='justify-content-end'>
-          <Navbar.Text>
-            Signed in as: <a href='#login'>Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
+  const logout = () => {
+    fire.auth().signOut()
+  }
+  return (
+    <div>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand href='#home'>MiniProjet covid19</Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#features'>Features</Nav.Link>
+          <Nav.Link href='#pricing'>Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <Button onClick={logout} variant='outline-info'>Logout</Button>
+        </Form>
       </Navbar>
-            <button onClick={logout}>Logout</button>
-        </div>
-    )
+    </div>
+  )
 }
-export default HomePage;
+export default HomePage
