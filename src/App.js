c49411429ceadc83cddc558de5fa61297fa1e55b
alonecheer <alonecheer@hotmail.com>
2020-04-22 22:03:02 +0700

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import LoginPage from './components/LoginPage'
+import { Route } from 'react-router-dom';
+import HomePage from './components/Homepage'
+
 function App() {
   return (
-    <div className="App">
-      <LoginPage/>
+    <div >
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/login" component={LoginPage} />
+      </div>
     </div>
   );
 }
