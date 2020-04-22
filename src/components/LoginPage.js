d2bf323f840bbd9958e5c9e1121c0d3058ebc3f0
alonecheer <alonecheer@hotmail.com>
2020-04-22 19:04:32 +0700

    loginform input button traget value

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,11 +1,25 @@
-import React,{ useState } from 'react'
-function LoginPage ()  {
-    const[username,setUsername]= useState('')
-    const[password,setPassword]= useState('')
-    return(
-        <div>
-            LoginPage
-        </div>
-    )
+import React, { useState } from 'react'
+function LoginPage () {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  console.log(username)
+  console.log(password)
+  return (
+    <div>
+      LoginPage
+      <div>
+        <input type='text' onChange={e => setUsername(e.target.value)}></input>
+      </div>
+      <div>
+        <input
+          type='password'
+          onChange={e => setPassword(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <button type='submit'>SingIn</button>
+      </div>
+    </div>
+  )
 }
 export default LoginPage
